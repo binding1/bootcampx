28881f43da35a00a6b6413608ac24d387a46aa68
binding1 <jaey.pro@gmail.com>
2023-01-11 23:11:39 +0000

    re-add assignments and assignment_submissions tables

--- migrations/assignment_submissions.sql
+++ migrations/assignment_submissions.sql
@@ -1,15 +1,15 @@
-CREATE TABLE assignment (
+CREATE TABLE assignments (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
+  name VARCHAR(255),
   content TEXT,
   day INTEGER,
   chapter INTEGER,
   duration INTEGER
 );
 
-CREATE TABLE assignments_submissions (
+CREATE TABLE assignment_submissions (
   id SERIAL PRIMARY KEY NOT NULL,
-  assignment_id INTEGER REFERENCES assignment(id) ON DELETE CASCADE,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
   student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
   duration INTEGER,
   submission_date DATE
